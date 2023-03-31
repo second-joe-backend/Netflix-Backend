e72a286306f6a03660da0b3e2336468ab435effe
dbtmdtt <dbtmdtt@gmail.com>
2023-03-31 11:27:24 +0900

    fav_movie

---
+++ netfilxProject/src/main/java/com/mysite/netfilxProject/NetfilxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.netfilxProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfilxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfilxProjectApplication.class, args);
+	}
+
+}
---
+++ netfilxProject/src/main/java/com/mysite/netfilxProject/favmovie/FavMovieVO.java
@@ -0,0 +1,8 @@
+package com.mysite.netfilxProject.favmovie;
+
+import lombok.Data;
+
+@Data
+public class FavMovieVO {
+
+}
