a7763988d6303f5a7b5e95b30177474c4540f99e
GilJuLee <dlrlfwn95@gmail.com>
2023-03-31 12:59:27 +0900

    fix : controller 주소 설정

--- netflixProject/src/main/java/com/mysite/netflixProject/board/BoardController.java
+++ netflixProject/src/main/java/com/mysite/netflixProject/board/BoardController.java
@@ -2,9 +2,11 @@ package com.mysite.netflixProject.board;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class BoardController {
 	
 	public BoardController(BoardServiceImpl impl) {
@@ -14,30 +16,31 @@ public class BoardController {
 
 	private BoardServiceImpl impl;
 	
+	@RequestMapping("/get")
 	public List<BoardVO> getBoardList() {
 		List<BoardVO> vo = impl.getBoardList();
 		return vo;
 	}
 
-	
+	@RequestMapping("/insert")
 	public int insertBoard(BoardVO board) {
 		int res = impl.insertBoard(board);
 		return res;
 	}
 
-	
+	@RequestMapping("/detail")
 	public BoardVO getDetail(BoardVO board) {
 		BoardVO vo = impl.getDetail(board);
 		return vo;
 	}
 
-	
+	@RequestMapping("/modify")
 	public int modifyBoard(BoardVO board) {
 		int res = impl.modifyBoard(board);
 		return res;
 	}
 
-	
+	@RequestMapping("/delete")
 	public int deleteBoard(BoardVO board) {
 		int res = impl.deleteBoard(board);
 		return res;
