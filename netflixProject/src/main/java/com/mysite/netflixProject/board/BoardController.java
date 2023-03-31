53544ec62be59a4a23794680e2cd4b10a25d8364
GilJuLee <dlrlfwn95@gmail.com>
2023-03-31 12:50:43 +0900

    feat : controller 제작

--- netflixProject/src/main/java/com/mysite/netflixProject/board/BoardController.java
+++ netflixProject/src/main/java/com/mysite/netflixProject/board/BoardController.java
@@ -1,9 +1,47 @@
 package com.mysite.netflixProject.board;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BoardController {
+	
+	public BoardController(BoardServiceImpl impl) {
+		super();
+		this.impl = impl;
+	}
+
+	private BoardServiceImpl impl;
+	
+	public List<BoardVO> getBoardList() {
+		List<BoardVO> vo = impl.getBoardList();
+		return vo;
+	}
+
+	
+	public int insertBoard(BoardVO board) {
+		int res = impl.insertBoard(board);
+		return res;
+	}
+
+	
+	public BoardVO getDetail(BoardVO board) {
+		BoardVO vo = impl.getDetail(board);
+		return vo;
+	}
+
+	
+	public int modifyBoard(BoardVO board) {
+		int res = impl.modifyBoard(board);
+		return res;
+	}
+
+	
+	public int deleteBoard(BoardVO board) {
+		int res = impl.deleteBoard(board);
+		return res;
+	}
 	
 	
 
