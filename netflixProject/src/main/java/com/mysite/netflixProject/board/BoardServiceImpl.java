b7001e001fadf6e73623c9856c5fd88ac6f5c318
GilJuLee <dlrlfwn95@gmail.com>
2023-03-31 12:48:53 +0900

    feat : BoardServiceimpl 제작

--- netflixProject/src/main/java/com/mysite/netflixProject/board/BoardServiceImpl.java
+++ netflixProject/src/main/java/com/mysite/netflixProject/board/BoardServiceImpl.java
@@ -1,15 +1,47 @@
 package com.mysite.netflixProject.board;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class BoardServiceImpl {
+public class BoardServiceImpl implements BoardService{
 	
 	private BoardMapper mapper;
 
 	public BoardServiceImpl(BoardMapper mapper) {
 		super();
 		this.mapper = mapper;
+	}
+
+	@Override
+	public List<BoardVO> getBoardList() {
+		List<BoardVO> vo = mapper.getBoardList();
+		return vo;
+	}
+
+	@Override
+	public int insertBoard(BoardVO board) {
+		int res = mapper.insertBoard(board);
+		return res;
+	}
+
+	@Override
+	public BoardVO getDetail(BoardVO board) {
+		BoardVO vo = mapper.getDetail(board);
+		return vo;
+	}
+
+	@Override
+	public int modifyBoard(BoardVO board) {
+		int res = mapper.modifyBoard(board);
+		return res;
+	}
+
+	@Override
+	public int deleteBoard(BoardVO board) {
+		int res = mapper.deleteBoard(board);
+		return res;
 	}
 
 
