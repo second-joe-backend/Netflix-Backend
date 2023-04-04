fa6b15b55a1f0f1db945cca3a4891cbeced68ba4
diana815 <diana815@naver.com>
2023-04-04 19:43:27 +0900

    feat: 회원 탈퇴 시 프로필 멤버 삭제하는 함수 구현

--- netflixProject/src/main/java/com/mysite/netflixProject/profile/ProfileService.java
+++ netflixProject/src/main/java/com/mysite/netflixProject/profile/ProfileService.java
@@ -2,6 +2,8 @@ package com.mysite.netflixProject.profile;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface ProfileService {
     public List<ProfileVO> getNicknames(ProfileVO vo);
     public ProfileVO getProfileDetail(ProfileVO vo);
@@ -9,5 +11,6 @@ public interface ProfileService {
     void insertNickname(String member_id, String nickname);
     int updateNickname(String member_id, int profile_id, String nickname);
     int deleteNickname(String member_id, int profile_id);
+    public int deleteProfileMember(ProfileVO vo);
 
 }
